9bcaf187bfe3de5b084685d4bca0ab68d344c66b
0.618 <o.6180339@gmail.com>
2021-05-01 16:34:17 -0700

    fix: add widget to top test and revert b6234cdca0

--- packages/application/src/shell.ts
+++ packages/application/src/shell.ts
@@ -338,7 +338,6 @@ export class LabShell extends Widget implements JupyterFrontEnd.IShell {
 
     // Skip Links
     const skipLinkWidget = (this._skipLinkWidget = new Widget());
-    skipLinkWidget.id = 'skip-link-widget';
     const skipToFilterFiles = document.createElement('a');
     const toggleSkipLink = (toShow: boolean) => {
       skipLinkWidget.node.classList.toggle('jp-skiplink--focus', toShow);
